6b6d68e834f5fe00efdc7186d2efe40c5571c6e4
BT_1N3_13 <bths@sample.edu>
2023-10-17 09:28:00 -0400

    finsihed distance slope and y int

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -10,4 +10,25 @@ public class LinearEquation {
         this.y1 = y1;
         this.y2 = y2;
     }
+
+
+    public double distance() {
+        double dx = Math.pow(x2-x1, 2);
+        double dy = Math.pow(y2-y1, 2);
+        return  Math.round(Math.sqrt(dx+dy * 100) / 100);
+    }
+
+    public double slope() {
+        double slope = (y2-y1) / (x2-x1);
+        return Math.round(slope * 100) / 100;
+    }
+
+    public double yIntercept() {
+        double slope = slope();
+        double b = y1 / (slope * x1);
+        return Math.round(b * 100) / 100;
+    }
+//    public String equation() {
+//
+//    }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -3,8 +3,10 @@ public class LinearEquationLogic {
     private String coordPair1;
     private String coordPair2;
     private Scanner myScanner;
+    private LinearEquation pair;
     public LinearEquationLogic() {
         myScanner = new Scanner(System.in);
+        pair = null;
     }
     public void start() {
         System.out.println("Welcome to the Linear equation calculator!");
@@ -21,7 +23,7 @@ public class LinearEquationLogic {
         coordPair2 = c2;
         int a2 = Integer.parseInt(c2.substring(1,c2.indexOf(",")));
         int b2 = Integer.parseInt(c2.substring(c2.indexOf(" ")+1, c2.indexOf(")")));
-        LinearEquation pair = new LinearEquation(a1,a2,b1,b2);
+        pair = new LinearEquation(a1,a2,b1,b2);
     }
 
     public void printInfo() {
